202d005007ba31e5c04375ff278105090537d441
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-29 01:12:18 +0900

    html

--- redux-lecture/src/App.jsx
+++ redux-lecture/src/App.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const ShowCount = styled.h1``;
+const Btn = styled.button``;
 
 class App extends Component {
   render() {
-    return <div>ss</div>;
+    return (
+      <Container>
+        <ShowCount>Count:</ShowCount>
+        <Btn>+</Btn>
+        <Btn>-</Btn>
+        <Btn>amount+</Btn>
+      </Container>
+    );
   }
 }
 
